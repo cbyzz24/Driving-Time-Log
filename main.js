d5132167b283b1f1e9c224d08fa4f42af14c4986
cbyzz24 <155403160+cbyzz24@users.noreply.github.com>
2025-06-11 17:29:43 -0600

    Added JS alert for invalid submit requests

--- main.js
+++ main.js
@@ -3,10 +3,14 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxkiioK3g6w0dS5UC7apZWb7q2iAmM1tjixWLs3Mr857SmIqFna1wpzA2GWZY7mLr0I/exec'
 const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg")
+const dateEl = document.getElementById("date")
+const dayMinsEl = document.getElementById("dayMins")
+const nightMinsEl = document.getElementById("nightMins")
 
 form.addEventListener('submit', e => {
 e.preventDefault()
-fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+if((dateEl.value != "") && (dayMinsEl.value != "" || nightMinsEl.value != "")){
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => {
         msg.innerHTML = "Time Submitted!"
         setTimeout(function(){
@@ -15,4 +19,7 @@ fetch(scriptURL, { method: 'POST', body: new FormData(form)})
         form.reset()
     })
     .catch(error => console.error('Error!', error.message))
+} else{
+    alert("Please enter Date and Day Minutes or Night Minutes")
+    }
 })
